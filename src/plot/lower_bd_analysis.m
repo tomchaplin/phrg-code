f61a52c9169b77fc2dc510b6e57d0af8f42129ae
Thomas Chaplin <tomrchaplin@gmail.com>
2021-08-17 16:15:59 +0100

    Improving bound analysis plots

--- src/plot/lower_bd_analysis.m
+++ src/plot/lower_bd_analysis.m
@@ -1,21 +1,32 @@
-ep = 0.1;
-max_L = 1000;
+ep=0.001;
 n = 5:5:200;
 critical_p = zeros(size(n));
 
 for i=1:length(n)
-    critical_p(i) = fzero(@(p) the_bound(n(i),p,ep,max_L) - 0.05, [10^(-6),0.2]);
+    critical_p(i) = fzero(@(p) the_bound(n(i),p,ep) - 0.05, [10^(-6),0.2]);
 end
 plot(n, critical_p)
 xlabel('$n$','Interpreter','latex')
 ylabel('$p_c(n)$','Interpreter','latex')
 
+% Plot our theoretical bound against empirical
+figure
+MODE='nreg';
+lower_betti1_plot
+hold on
+plot(n(4:30), critical_p(4:30))
+legend({'Empirical $p=p_l(n)$',...
+    sprintf('Best fit $p= %.3f \\,n^{%.3f}$', exp(intercept), slope),...
+    'Theory $p=p_c(n)$'}, 'Interpreter','latex')
+ylabel('$p$','Interpreter','latex')
+
+
 % Probability bound that should work at small densities
-function sum = the_bound(n, p, ep, max_L)
+function sum = the_bound(n, p, ep)
     sum = 0;
     err = ep*2;
     L=2;
-    while err > ep && L<=max_L && L<=n
+    while err > ep && L<=n
         % Save
         old_sum = sum;
         % Compute next
